a4578588cf7d5f1bff291b41712e22e48ef70395
Eddie Lebow <elebow@users.noreply.github.com>
2018-04-12 00:31:47 -0400

    Put main.py logic into a class

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 
 import json
+import sys
 
 from submission_collector import SubmissionCollector
 
 
+class Main:
+    def __init__(self):
+        self.load_config()
+        self.submission_collector = SubmissionCollector(app=self.app,
+                                                        secret=self.secret,
+                                                        user_agent=self.user_agent)
+
+    def author_submissions_to_json(self, author_name):
+        submissions = self.submission_collector.all_submissions_by_author_name(author_name)
+        print(json.dumps(submissions))
+
+    def load_config(self):
+        import configparser
+        cfg = configparser.ConfigParser()
+        cfg.read(sys.argv[1])
+
+        self.app = cfg.get('DEFAULT', 'app')
+        self.secret = cfg.get('DEFAULT', 'secret')
+        self.user_agent = cfg.get("DEFAULT", "user_agent")
+
+
 if __name__ == "__main__":
-    import configparser
-    import sys
-
-    cfg = configparser.ConfigParser()
-    cfg.read(sys.argv[1])
-    app = cfg.get('DEFAULT', 'app')
-    secret = cfg.get('DEFAULT', 'secret')
-    user_agent = cfg.get("DEFAULT", "user_agent")
-
-    subm_collector = SubmissionCollector(app=app,
-                                         secret=secret,
-                                         user_agent=user_agent)
-    print(json.dumps(subm_collector.all_submissions_by_author_name(sys.argv[2])))
+    Main().author_submissions_to_json(sys.argv[2])
