6b43b93e123df4c092f1ef9fea91ced1086f55de
Eddie Lebow <elebow@users.noreply.github.com>
2018-04-12 00:25:07 -0400

    Move submission collection logic to new class.
    Except for the JSON responsibility. That belongs in main, for now.
    
    Also, be explicit about how we select wanted attributes of comment and
    submission objects.

--- series_to_epub.py
+++ series_to_epub.py
@@ -1,45 +1,8 @@
 #!/usr/bin/python3
 
 import json
-import praw
 
-
-class SubmissionCollector:
-    def __init__(self, *, app, secret, user_agent, target_name):
-        self.reddit = praw.Reddit(client_id=app, client_secret=secret, user_agent=user_agent)
-        self.target = self.reddit.redditor(target_name)
-
-    def all_submissions_as_json(self):
-        return json.dumps(
-            [{"post": self.filter_subm(subm), "comments": self.all_comments_for_subm(subm)}
-                for subm
-                in self.target.submissions.new(limit=3)]
-        )
-
-    def all_comments_for_subm(self, subm):
-        subm.comments.replace_more(limit=None)
-        return [self.comment_tree(comm) for comm in subm.comments]
-
-    def comment_tree(self, comm):
-        if len(comm.replies) == 0:
-            return self.filter_comm(comm)
-        else:
-            return {
-                "comment": self.filter_comm(comm),
-                "replies": [self.comment_tree(c) for c in comm.replies]
-            }
-
-    def filter_comm(self, comm):
-        comm.__dict__["author"] = comm.author.name  # replace author obj with a string
-        #if comm.__dict__["edited"] is False:
-        #    del comm.__dict__["edited"]
-        wanted_keys = ["author", "created_utc", "body", "edited", "permalink"]
-        return {k: v for k, v in comm.__dict__.items() if k in wanted_keys}
-
-    def filter_subm(self, subm):
-        subm.__dict__["author"] = subm.author.name  # replace author obj with a string
-        wanted_keys = ["author", "title", "created_utc", "selftext", "edited", "permalink"]
-        return {k: v for k, v in subm.__dict__.items() if k in wanted_keys}
+from submission_collector import SubmissionCollector
 
 
 if __name__ == "__main__":
@@ -54,6 +17,5 @@ if __name__ == "__main__":
 
     subm_collector = SubmissionCollector(app=app,
                                          secret=secret,
-                                         user_agent=user_agent,
-                                         target_name=sys.argv[2])
-    print(subm_collector.all_submissions_as_json())
+                                         user_agent=user_agent)
+    print(json.dumps(subm_collector.all_submissions_by_author_name(sys.argv[2])))
---
+++ submission_collector.py
@@ -0,0 +1,61 @@
+import praw
+
+
+class SubmissionCollector:
+    def __init__(self, *, app, secret, user_agent):
+        self.reddit = praw.Reddit(client_id=app, client_secret=secret, user_agent=user_agent)
+
+    def all_submissions_by_author_name(self, author_name):
+        author = self.reddit.redditor(author_name)
+        return [self.submission_details(subm) for subm in author.submissions.new(limit=3)]
+
+    def all_submissions_in_list_of_ids(self, id_list):
+        # TODO
+        pass
+
+    def all_submissions_mentioned_in_reddit_thing(self, thing_id):
+        # TODO
+        pass
+
+    def all_submissions_mentioned_in_reddit_wiki_page(self, page):
+        # TODO
+        pass
+
+    def submission_details(self, subm):
+        return {
+            "post": self.filter_subm(subm),
+            "comments": self.all_comments_for_subm(subm)
+        }
+
+    def all_comments_for_subm(self, subm):
+        subm.comments.replace_more(limit=None)
+        return [self.comment_tree(comm) for comm in subm.comments]
+
+    def comment_tree(self, comm):
+        if len(comm.replies) == 0:
+            return self.filter_comm(comm)
+        else:
+            return {
+                "comment": self.filter_comm(comm),
+                "replies": [self.comment_tree(c) for c in comm.replies]
+            }
+
+    def filter_comm(self, comm):
+        return {
+            "author_name": comm.author.name,
+            "author_flair_text": comm.author_flair_text,
+            "body": comm.body,
+            "created_utc": comm.created_utc,
+            "edited": comm.edited,
+            "permalink": comm.permalink
+        }
+
+    def filter_subm(self, subm):
+        return {
+            "author_name": subm.author.name,
+            "created_utc": subm.created_utc,
+            "edited": subm.edited,
+            "permalink": subm.permalink,
+            "selftext": subm.selftext,
+            "title": subm.title,
+        }
