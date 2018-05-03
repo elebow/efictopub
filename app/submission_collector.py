2785f4abdb0371ac10c84077e865902ad0258a1b
Eddie Lebow <elebow@users.noreply.github.com>
2018-05-02 23:31:49 -0400

    Use namedtuples instead of dicts for reddit threads
    Still not sure whether these should be classes.

--- app/submission_collector.py
+++ app/submission_collector.py
@@ -1,13 +1,34 @@
 import praw
 
+from collections import namedtuple
+
 
 class SubmissionCollector:
+    Submission = namedtuple("Submission",
+                            ["author_name",
+                             "comments",
+                             "created_utc",
+                             "edited",
+                             "permalink",
+                             "selftext",
+                             "title",
+                             "ups"])
+    Comment = namedtuple("Comment",
+                         ["author_name",
+                          "author_flair_text",
+                          "body",
+                          "created_utc",
+                          "edited",
+                          "permalink",
+                          "replies",
+                          "ups"])
+
     def __init__(self, *, app, secret, user_agent):
         self.reddit = praw.Reddit(client_id=app, client_secret=secret, user_agent=user_agent)
 
     def all_submissions_by_author_name(self, author_name):
         author = self.reddit.redditor(author_name)
-        return [self.submission_details(subm) for subm in author.submissions.new(limit=3)]
+        return [self.extract_subm_attrs(subm) for subm in author.submissions.new(limit=3)]
 
     def all_submissions_in_list_of_ids(self, id_list):
         # TODO
@@ -28,43 +49,32 @@ class SubmissionCollector:
         # TODO
         pass
 
-    def submission_details(self, subm):
-        return {
-            "post": self.filter_subm(subm),
-            "comments": self.all_comments_for_subm(subm)
-        }
-
-    def all_comments_for_subm(self, subm):
+    def all_comments_for_submission(self, subm):
         subm.comments.replace_more(limit=None)
-        return [self.comment_tree(comm) for comm in subm.comments]
+        return [self.extract_comm_attrs(comm) for comm in subm.comments]
 
-    def comment_tree(self, comm):
-        if len(comm.replies) == 0:
-            return self.filter_comm(comm)
-        else:
-            return {
-                "comment": self.filter_comm(comm),
-                "replies": [self.comment_tree(c) for c in comm.replies]
-            }
+    def replies_for_comment(self, comm):
+        return [self.extract_comm_attrs(c) for c in comm.replies]
 
-    def filter_comm(self, comm):
-        return {
-            "author_name": comm.author.name if comm.author else "[n/a]",
-            "author_flair_text": comm.author_flair_text,
-            "body": comm.body,
-            "created_utc": comm.created_utc,
-            "edited": comm.edited,
-            "permalink": comm.permalink,
-            "ups": comm.ups
-        }
+    def extract_comm_attrs(self, comm):
+        return self.Comment(author_name=comm.author.name if comm.author else "[n/a]",
+                            author_flair_text=comm.author_flair_text,
+                            body=comm.body,
+                            created_utc=comm.created_utc,
+                            edited=comm.edited,
+                            replies=self.replies_for_comment(comm),
+                            permalink=comm.permalink,
+                            ups=comm.ups)
 
-    def filter_subm(self, subm):
-        return {
-            "author_name": subm.author.name,
-            "created_utc": subm.created_utc,
-            "edited": subm.edited,
-            "permalink": subm.permalink,
-            "selftext": subm.selftext,
-            "title": subm.title,
-            "ups": subm.ups
-        }
+    def extract_subm_attrs(self, subm):
+        return self.Submission(author_name=subm.author.name,
+                               comments=self.all_comments_for_submission(subm),
+                               created_utc=subm.created_utc,
+                               edited=subm.edited,
+                               permalink=subm.permalink,
+                               selftext=subm.selftext,
+                               title=subm.title,
+                               ups=subm.ups)
+
+    def setup_reddit(self, app, secret, user_agent):
+        self.reddit = praw.Reddit(client_id=app, client_secret=secret, user_agent=user_agent)
