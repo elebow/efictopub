76a0a55e499d0840cbd470731910030873ffa017
Eddie Lebow <elebow@users.noreply.github.com>
2018-05-20 00:33:09 -0400

    Add id to Book

--- app/book.py
+++ app/book.py
@@ -1,5 +1,15 @@
+import urllib.parse
+
+
 class Book:
     def __init__(self, *, title, chapters):
         self.title = title
         self.chapters = chapters
         self.author = chapters[0].author  # assume all the chapters have the same author
+        self.date_start = chapters[0].created_utc
+        self.date_end = chapters[-1].created_utc
+
+        self.id = self.calculate_id()
+
+    def calculate_id(self):
+        return urllib.parse.quote_plus(self.date_start + self.chapters[0].permalink)
