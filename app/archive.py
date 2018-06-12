d7e0790a64e880f8e445709b140615318f114546
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-12 00:29:42 -0400

    Add representers and constructors for Chapter and Comment
    And rely on the new Story.as_dict() in the Story representer.

--- app/archive.py
+++ app/archive.py
@@ -2,6 +2,8 @@ import os
 import yaml
 
 from app.models.story import Story
+from app.models.chapter import Chapter
+from app.models.comment import Comment
 
 
 class Archive:
@@ -10,7 +12,8 @@ class Archive:
     @staticmethod
     def get(id):
         # TODO accept partial ids, as long as they map to a unique file
-        # TODO if id contains a slash, assume it is a path rather than an id. No, use a more reliable heuristic than presence of a slash
+        # TODO if id contains a slash, assume it is a path rather than an id.
+        #    No, use a more reliable heuristic than presence of a slash
         with open(Archive.path(id), "r") as infile:
             return yaml.safe_load(infile)
 
@@ -26,11 +29,7 @@ class Archive:
 
 def story_representer(dumper, story):
     """Represent a Story object in a way that can be encoded as YAML"""
-    mapping = {
-        "title": story.title,
-        "chapters": [chapter.as_dict() for chapter in story.chapters]
-    }
-    return dumper.represent_mapping(u"!story", mapping)
+    return dumper.represent_mapping(u"!story", story.as_dict())
 
 
 def story_constructor(loader, node):
@@ -43,3 +42,33 @@ def story_constructor(loader, node):
 
 yaml.SafeDumper.add_representer(Story, story_representer)
 yaml.SafeLoader.add_constructor(u'!story', story_constructor)
+
+
+def chapter_representer(dumper, chapter):
+    """Represent a Chapter object in a way that can be encoded as YAML"""
+    return dumper.represent_mapping(u"!chapter", chapter.as_dict())
+
+
+def chapter_constructor(loader, node):
+    """Build a Chapter object from a representation that can be encoded as YAML"""
+    mapping = loader.construct_mapping(node)
+    return Chapter.from_dict(mapping)
+
+
+yaml.SafeDumper.add_representer(Chapter, chapter_representer)
+yaml.SafeLoader.add_constructor(u'!chapter', chapter_constructor)
+
+
+def comment_representer(dumper, comment):
+    """Represent a Comment object in a way that can be encoded as YAML"""
+    return dumper.represent_mapping(u"!comment", comment.as_dict())
+
+
+def comment_constructor(loader, node):
+    """Build a Comment object from a representation that can be encoded as YAML"""
+    mapping = loader.construct_mapping(node)
+    return Comment.from_dict(mapping)
+
+
+yaml.SafeDumper.add_representer(Comment, comment_representer)
+yaml.SafeLoader.add_constructor(u'!comment', comment_constructor)
