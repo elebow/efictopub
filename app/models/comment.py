c3418dd42c21098be54fc4f40f19669cac46b475
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-12 00:21:47 -0400

    Add Comment class
    A fetcher-agnostic comment, akin to Chapter.

---
+++ app/models/comment.py
@@ -0,0 +1,35 @@
+class Comment:
+    """
+    A comment on a chapter.
+    """
+
+    def __init__(self, *,
+                 author,
+                 date_published,
+                 date_updated,
+                 permalink,
+                 replies,
+                 score,
+                 text):
+        self.author = author
+        self.date_published = date_published
+        self.date_updated = date_updated
+        self.permalink = permalink
+        self.replies = replies
+        self.score = score
+        self.text = text
+
+    def as_dict(self):
+        attr_names = ["author", "date_published", "date_updated", "permalink", "replies", "score", "text"]
+        mapping = {name: getattr(self, name) for name in attr_names}
+        return mapping
+
+    @classmethod
+    def from_dict(cls, mapping):
+        return cls(author=mapping["author"],
+                   date_published=mapping["date_published"],
+                   date_updated=mapping["date_updated"],
+                   permalink=mapping["permalink"],
+                   replies=mapping["replies"],
+                   score=mapping["score"],
+                   text=mapping["text"])
