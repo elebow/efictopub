fa51d736ad3a04ea0904cc002a36977e075bd7a7
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-15 02:18:10 -0400

    Update Main test for new generic Chapter

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,7 +17,7 @@ class TestMain:
         subject = Main(args)
         story = subject.get_story()
 
-        assert [chap.get_text() for chap in story.chapters] == ["chapter 0", "chapter 1", "chapter 2"]
+        assert [chap.text for chap in story.chapters] == ["chapter 0", "chapter 1", "chapter 2"]
         assert story.author_name == "great author 0"
         assert story.date_start == "start date 0"
         assert story.date_end == "end date 2"
