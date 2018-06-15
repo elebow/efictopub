a39061b9400721709d4d6e9d7f0fd58100e4989c
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-15 01:02:41 -0400

    Move test fixtures to their own module.
    Another module will provide doubles
    
    I'm using ad-hoc functions in a module instead of pytest's built-in
    fixtures facility. I gave pytest a shot, and I just don't like it. Maybe
    I'm using it wrong. This seems plain and straightforward.

---
+++ tests/fixtures/real.py
@@ -0,0 +1,31 @@
+from app.models.chapter import Chapter
+from app.models.comment import Comment
+
+
+def chapter_real(n=0):
+    return Chapter(comments=comments_real(3),
+                   date_published=f"start_date {n}",
+                   date_updated=f"end_date {n}",
+                   permalink=f"permalink {n}",
+                   author=f"great-author {n}",
+                   score=5,
+                   text=f"chapter {n}",
+                   title=f"great-title {n}")
+
+
+def chapters_real(count):
+    return [chapter_real(n) for n in range(0, count)]
+
+
+def comment_real(n):
+    return Comment(author=f"author {n}",
+                   date_published=f"published date {n}",
+                   date_updated=f"updated date {n}",
+                   permalink=f"permalink {n}",
+                   replies=[comment_real(m) for m in range(0, n - 1)] if n > 0 else [],
+                   score=f"score {n}",
+                   text=f"text {n}")
+
+
+def comments_real(count):
+    return [comment_real(n) for n in range(0, count)]
--- tests/test_story.py
+++ tests/test_story.py
@@ -1,26 +1,16 @@
-import pytest
-from unittest.mock import MagicMock
-
 from app.models.story import Story
 
-
-@pytest.fixture()
-def chapters():
-    return [
-        MagicMock("Chapter", get_text=lambda: "chapter 1", created_utc="start_date", permalink="permalink",
-                  author_name="great-author", title="great-title"),
-        MagicMock("Chapter", get_text=lambda: "chapter 2"),
-        MagicMock("Chapter", get_text=lambda: "chapter 3", created_utc="end_date")
-    ]
+from tests.fixtures.real import chapters_real
 
 
 class TestStory:
 
     def setup_method(self):
-        self.subject = Story(chapters=chapters())
+        chapters = chapters_real(3)
+        self.subject = Story(chapters=chapters)
 
     def test_calculate_id(self):
-        assert self.subject.id == "start_datepermalink"
+        assert self.subject.id == "start_date+0permalink+0"
 
     def test_calculate_title(self):
-        assert self.subject.title == "great-title"
+        assert self.subject.title == "great-title 0"
