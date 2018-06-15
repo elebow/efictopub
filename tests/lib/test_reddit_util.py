d60a785fde6bddb992308d66a775c9d17e8bca89
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-15 02:29:37 -0400

    Update reddit_util test to use common fixtures

--- tests/lib/test_reddit_util.py
+++ tests/lib/test_reddit_util.py
@@ -7,12 +7,7 @@ from app.lib import reddit_util
 from app.models import reddit
 from app import exceptions
 
-
-@pytest.fixture
-def praw_submissions():
-    import pickle
-    with open("tests/fixtures/array_of_3_submissions_with_comments.pickle", "rb") as file:
-        return pickle.load(file)
+from tests.fixtures.real import praw_submissions_real
 
 
 class TestRedditUtil:
@@ -22,16 +17,16 @@ class TestRedditUtil:
         self.praw_reddit = MagicMock()
 
     @patch("app.lib.reddit_util.parse_url")
-    def test_parse_thing_or_id_or_url(self, parse_url, praw_submissions):
-        output_praw_subm = self.subject.parse_thing_or_id_or_url(praw_submissions[0], self.praw_reddit)
+    def test_parse_thing_or_id_or_url(self, parse_url):
+        output_praw_subm = self.subject.parse_thing_or_id_or_url(praw_submissions_real()[0], self.praw_reddit)
         assert isinstance(output_praw_subm, reddit.Submission)
-        assert len(output_praw_subm.comments) == len(praw_submissions[0].comments)
+        assert len(output_praw_subm.comments) == len(praw_submissions_real()[0].comments)
 
-        output_praw_comm = self.subject.parse_thing_or_id_or_url(praw_submissions[0].comments[0],
+        output_praw_comm = self.subject.parse_thing_or_id_or_url(praw_submissions_real()[0].comments[0],
                                                                  self.praw_reddit)
         assert isinstance(output_praw_comm, reddit.Comment)
 
-        #output_praw_wiki = self.subject.parse_thing_or_id_or_url(praw_submissions[0].comments[0])
+        #output_praw_wiki = self.subject.parse_thing_or_id_or_url(praw_submissions_real()[0].comments[0])
         #assert isinstance(output_praw_wiki, WikiPage) #TODO
 
         with pytest.raises(exceptions.AmbiguousIdError):
