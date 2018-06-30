21851b0ef6f5bbe83368a11cf755d92277780160
Eddie Lebow <elebow@users.noreply.github.com>
2018-06-30 00:46:44 -0400

    Stub requests.get by default.
    The method, defined in tests/fixtures/stubs.py, assumes that
    `tests.fixtures.stubs.return_values` will be populated by the calling
    test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
 import os
 import pytest
+import requests
 from unittest.mock import MagicMock
 
 import app.lib.reddit_util
+import tests.fixtures.stubs
 
-if os.environ.get("LIVE_REDDIT") != "true":
+if os.environ.get("LIVE_REQUESTS") != "true":
     @pytest.fixture(autouse=True, scope="session")
-    def no_hit_reddit():
+    def no_reddit():
         app.lib.reddit_util.setup_reddit = MagicMock()
+
+    @pytest.fixture(autouse=True, scope="session")
+    def no_requests():
+        requests.get = tests.fixtures.stubs.request_get
---
+++ tests/fixtures/stubs.py
@@ -0,0 +1,3 @@
+def request_get(url):
+    # tests.fixtures.stubs.return_values must be populated by the consumer
+    return return_values.pop(0)
