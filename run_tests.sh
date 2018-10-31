a036e83b24197002bb68502e12c835e93e5a746c
Eddie Lebow <elebow@users.noreply.github.com>
2018-10-30 21:49:05 -0400

    Pass all arguments to pytest in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pipenv run python3 -m pytest -s --cov=app
+pipenv run python3 -m pytest -s --cov=app "$@"
 # -s         do not capture output. This is needed for ipdb.set_trace()
 # --cov=app  show code coverage for the dir `app`
