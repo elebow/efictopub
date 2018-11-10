58c37f6dc001e1af5eea28e1704ea393e0bf384e
Eddie Lebow <elebow@users.noreply.github.com>
2018-11-10 01:18:30 -0500

    Break up parsing method in FFNetChapter
    Also add two new methods, is_last_chapter() and
    is_single_chapter_story(), for use by the FFNet fetcher to determine
    when to stop requesting pages.

--- app/models/ffnet/ffnet_chapter.py
+++ app/models/ffnet/ffnet_chapter.py
@@ -12,18 +12,8 @@ class FFNetChapter:
 
     def set_fields_from_html(self, chapter_html, reviews_htmls):
         self.dom = bs4.BeautifulSoup(chapter_html, "lxml")
-        info_box = self.dom.select("#profile_top")[0]
-        info_fields = info_box.select(".xcontrast_txt")
 
-        self.author_name = info_fields[2].text.strip()
-        self.story_title = info_fields[0].text.strip()
-        self.summary = info_fields[5]  # TODO do something with this
-        try:
-            self.cover_image_url = info_box.select("img")[0].attrs["src"]  # TODO do something with this
-        except IndexError:
-            self.cover_image_url = ""
-
-        self.score_dates_id = str(info_fields[6])
+        self.score_dates_id = str(self.get_info_fields()[6])
 
         storytext_with_container = str(self.dom.select("#storytext")[0])
         # Yes, use regex to work with HTML. It's a very constrained input.
@@ -33,6 +23,16 @@ class FFNetChapter:
         self.text = text_maker.handle(storytext_html).strip()
 
         self.reviews = reviews_htmls  # TODO convert to text
+
+    @functools.lru_cache()
+    def get_info_fields(self):
+        info_box = self.dom.select("#profile_top")
+        if not info_box:
+            return ["unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown"]
+        return info_box[0].select(".xcontrast_txt")
+
+    def get_author_name(self):
+        return self.get_info_fields()[2].text.strip()
 
     def get_chapter_title(self):
         select = self.dom.select("#chap_select")
@@ -57,14 +57,42 @@ class FFNetChapter:
     def get_score(self):
         return re.search(r"Favs:\s+([\d,]+)", self.score_dates_id, re.DOTALL).group(1)
 
+    def get_story_title(self):
+        return self.get_info_fields()[0].text.strip()
+
+    def get_summary(self):
+        # TODO do something with this
+        return self.get_info_fields()[5].text.strip()
+
+    @functools.lru_cache()
+    def get_chapter_selector(self):
+        return self.dom.select("#chap_select")
+
+    def is_last_chapter(self):
+        # Return True if the last chapter in the chapter selector is the one that is selected
+        if self.is_single_chapter_story():
+            return False
+
+        select = self.get_chapter_selector()[0]
+        chapters = [chap for chap in select.children
+                    if isinstance(chap, bs4.element.Tag)]
+
+        if len(chapters) == 1:
+            return True
+
+        return 'selected' in chapters[-1].attrs
+
+    def is_single_chapter_story(self):
+        return not self.get_chapter_selector()
+
     @functools.lru_cache()
     def as_chapter(self):
-        return Chapter(author=self.author_name,
+        return Chapter(author=self.get_author_name(),
                        comments=self.reviews,
                        date_published=self.get_date_published(),
                        date_updated=self.get_date_updated(),
                        permalink=self.get_permalink(),
                        score=self.get_score(),
-                       story_title=self.story_title,
+                       story_title=self.get_story_title(),
                        text=self.text,
                        title=self.get_chapter_title())
