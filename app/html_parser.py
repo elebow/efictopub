73e99ec9e2825938d4c4846bb83a2f44c4aa795c
Eddie Lebow <elebow@users.noreply.github.com>
2019-01-03 00:11:22 -0500

    Add HTMLParser
    This is the first step in changing the internal story format from
    Markdown to HTML.
    
    Markdown made sense when this app was reddit-only, but most sites use
    HTML as their native format and include things that Markdown cannot
    represent (like centered text).
    
    Markdown doesn't actually provide any substantial benefits over HTML
    (easier editing of the source directly, maybe?) and the reddit API
    also provides HTML, so we'll just use that for everything.

---
+++ app/html_parser.py
@@ -0,0 +1,28 @@
+import bs4
+import functools
+from collections import namedtuple
+import re
+
+
+class HTMLParser:
+    Link = namedtuple("Link", ["href", "text", "title"])
+
+    def __init__(self, src):
+        self.dom = bs4.BeautifulSoup(src, "lxml")
+
+    @property
+    @functools.lru_cache()
+    def links(self):
+        return [self.Link(href=a.attrs.get("href"), text=a.text, title=a.attrs.get("title"))
+                for a
+                in self.dom.select("a")]
+
+    def as_html(self):
+        self.dom.encode()
+
+    # Return all the links contaning a specified string.
+    # This is useful for finding prev/next links in a post that is part of a series.
+    # Note that link text is already downcased in parse_links().
+    def links_containing_text(self, text_to_find):
+        regexp_to_find = re.compile(r"\b%s\b" % text_to_find, re.IGNORECASE)
+        return [link for link in self.links if regexp_to_find.search(link.text)]
---
+++ tests/test_html_parser.py
@@ -0,0 +1,35 @@
+from app.html_parser import HTMLParser
+
+
+class TestHTMLParser:
+    def setup_method(self):
+        self.src = """
+here is some html
+<a href="example.com/first">Prev</a>
+<a href="example.com/second" title="title goes here">click here for next, with a title</a>
+<a href="example.com/third">link with <em>formatting</em> in it</a>
+<a href="example.com/not_next">snexto</a>
+
+"""
+        self.parser = HTMLParser(self.src)
+
+    def test_parse_links(self):
+        assert(len(self.parser.links) == 4)
+
+        assert(self.parser.links[0].href == "example.com/first")
+        assert(self.parser.links[0].text == "Prev")
+
+        assert(self.parser.links[1].href == "example.com/second")
+        assert(self.parser.links[1].text == "click here for next, with a title")
+        assert(self.parser.links[1].title == "title goes here")
+
+        assert(self.parser.links[2].text == "link with formatting in it")
+
+    def test_links_containing_text(self):
+        next_links = self.parser.links_containing_text("next")
+        assert(len(next_links) == 1)
+        assert(next_links[0].text == "click here for next, with a title")
+
+        prev_links = self.parser.links_containing_text("prev")
+        assert(len(prev_links) == 1)
+        assert(prev_links[0].text == "Prev")
