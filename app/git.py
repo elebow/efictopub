253ae0f7bb03c28834b9614fb0d69f12cc5e86b6
Eddie Lebow <elebow@users.noreply.github.com>
2019-02-19 01:17:52 -0500

    Add functions to check whether git repo has been modified

--- app/git.py
+++ app/git.py
@@ -1,3 +1,4 @@
+import dulwich
 import dulwich.porcelain
 
 from app import archive
@@ -6,7 +7,7 @@ from app import config
 """Commits archived files to local git repo."""
 
 repo_path = config.archive.location
-author = "efictopub <efictopub@users.noreply.github.com>"
+efic_author = "efictopub <efictopub@users.noreply.github.com>"
 
 
 def commit_story(story, commit_msg='Update story'):
@@ -14,7 +15,25 @@ def commit_story(story, commit_msg='Update story'):
     ensure_repo_initialized()
 
     dulwich.porcelain.add(repo_path, filename)
-    dulwich.porcelain.commit(repo_path, message=commit_msg, author=author)
+    dulwich.porcelain.commit(repo_path, message=commit_msg, author=efic_author)
+
+
+def repo_is_dirty():
+    """Return true if the git repo has any uncommitted changes, which we don't want to clobber."""
+    status = dulwich.porcelain.status(repo_path)
+    return len(status.staged["add"]) > 0 or \
+        len(status.staged["delete"]) > 0 or \
+        len(status.staged["modify"]) > 0 or \
+        len(status.unstaged) > 0 or \
+        len(status.untracked) > 0
+
+
+def previous_commit_is_not_efic(file):
+    authors = [entry.commit.author
+               for entry
+               in dulwich.repo.Repo(".").get_walker(paths=[file], max_entries=2)]
+
+    return any([author != efic_author for author in authors])
 
 
 def ensure_repo_initialized():
--- tests/test_git.py
+++ tests/test_git.py
@@ -1,3 +1,6 @@
+from dulwich.porcelain import GitStatus
+from dulwich.walk import WalkEntry
+
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
@@ -23,3 +26,28 @@ class TestGit:
         commit.assert_called_once_with("/path/to/archive",
                                        message="Update story",
                                        author="efictopub <efictopub@users.noreply.github.com>")
+
+    @patch("app.git.repo_path", "/path/to/archive")
+    @patch("dulwich.porcelain.status", lambda _x: GitStatus(staged={'add': [], 'delete': [], 'modify': []},
+                                                            unstaged=[b'f1'],
+                                                            untracked=[]))
+    def test_repo_is_dirty_dirty(self):
+        assert git.repo_is_dirty() is True
+
+    @patch("app.git.repo_path", "/path/to/archive")
+    @patch("dulwich.porcelain.status", lambda _x: GitStatus(staged={'add': [], 'delete': [], 'modify': []},
+                                                            unstaged=[],
+                                                            untracked=[]))
+    def test_repo_is_dirty_clean(self):
+        assert git.repo_is_dirty() is False
+
+    @patch("dulwich.repo.Repo.get_walker",
+           lambda _x, **args: [MagicMock(commit=MagicMock(author="some other committer"))])
+    def test_previous_commit_is_not_efic_true(self):
+        assert git.previous_commit_is_not_efic("_file") is True
+
+    @patch("dulwich.repo.Repo.get_walker",
+           lambda _x, **args: [MagicMock(commit=MagicMock(
+               author="efictopub <efictopub@users.noreply.github.com>"))])
+    def test_previous_commit_is_not_efic_false(self):
+        assert git.previous_commit_is_not_efic("_file") is False
