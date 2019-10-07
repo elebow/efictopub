cc9dda90c6db5481669d121624b27b192656e6e9
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-07 00:10:33 -0400

    Add example client app to docs/

---
+++ docs/example_client_app.py
@@ -0,0 +1,18 @@
+from efictopub import Efictopub
+
+args = {
+    "target": "http://www.reddit.com/r/whatever/comments/t5555/my_great_story",
+    "title": "My Great Story",
+}
+
+# instantiate the Efictopub object. This fetches the story.
+efictopub = Efictopub(args)
+
+# write the story to the archive
+efictopub.archive_story()
+
+# write an EPUB file
+efictopub.write_epub()
+
+# return EPUB file contents without writing to disk
+efictopub.epub()
