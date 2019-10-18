ee5706901183c0fad5583d9123ffc99a57c13543
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-17 21:33:36 -0400

    Update test_reddit_author to use new test fixtures
    Actually a bunch of ad-hoc mocks, because PRAW is hard to mock. This
    is hardly better than what we had before, but at least it's confined
    to this one file.

--- tests/fetchers/test_reddit_author.py
+++ tests/fetchers/test_reddit_author.py
@@ -3,25 +3,41 @@ from doubles import allow
 from efictopub import config
 from efictopub.fetchers import reddit_author
 
-from tests.fixtures.doubles import praw_redditor_with_submissions_double
+from tests.fixtures.real import get_reddit_submission
 
 
 class TestFetchersRedditAuthor:
-    def test_submissions_by_author(self):
-        fetcher = reddit_author.Fetcher("reddit.com/u/some_redditor")
-        allow(fetcher.praw_reddit).redditor.and_return(
-            praw_redditor_with_submissions_double()
+    def test_submissions_by_author(self, mocker):
+        mock_praw_submissions = [
+            mocker.Mock(title="PRAW Submission 00", id="000000"),
+            mocker.Mock(title="PRAW Submission 01", id="000001"),
+            mocker.Mock(title="PRAW Submission 02", id="000002"),
+        ]
+        mock_redditor = mocker.Mock(
+            submissions=mocker.Mock(new=mocker.Mock(return_value=mock_praw_submissions))
         )
+        mock_reddit = mocker.Mock(redditor=mocker.Mock(return_value=mock_redditor))
+        mocker.patch("efictopub.lib.reddit_util.setup_reddit", return_value=mock_reddit)
 
+        fetcher = reddit_author.Fetcher("reddit.com/u/some_redditor")
         subms = fetcher.fetch_submissions()
+
         assert [subm.reddit_id for subm in subms] == ["000000", "000001", "000002"]
 
-    def test_submissions_by_author_with_pattern(self):
-        config.config["fetcher_opts"] = ["title_pattern=0\\d"]
-        fetcher = reddit_author.Fetcher("reddit.com/u/some_redditor")
-        allow(fetcher.praw_reddit).redditor.and_return(
-            praw_redditor_with_submissions_double()
+    def test_submissions_by_author_with_pattern(self, mocker):
+        mock_praw_submissions = [
+            mocker.Mock(title="PRAW Submission 00", id="000000"),
+            mocker.Mock(title="PRAW Submission 01", id="000001"),
+            mocker.Mock(title="PRAW Submission 02", id="000002"),
+        ]
+        mock_redditor = mocker.Mock(
+            submissions=mocker.Mock(new=mocker.Mock(return_value=mock_praw_submissions))
         )
+        mock_reddit = mocker.Mock(redditor=mocker.Mock(return_value=mock_redditor))
+        mocker.patch("efictopub.lib.reddit_util.setup_reddit", return_value=mock_reddit)
+        config.config["fetcher_opts"] = ["title_pattern=[^0]\\Z"]
 
+        fetcher = reddit_author.Fetcher("reddit.com/u/some_redditor")
         subms = fetcher.fetch_submissions()
+
         assert [subm.reddit_id for subm in subms] == ["000001", "000002"]
