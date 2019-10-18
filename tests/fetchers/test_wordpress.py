93bb790aee5d3d5ef177a434214226dc7f1dfe94
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-18 00:50:06 -0400

    Use requests_mock instead of homegrown in test_wordpress.py
    I was actually quite satisfied with the homegrown approach and was
    planning to expand it to match URLs. But, requests_mock is
    well-established and seems to already do exactly what I need.

--- tests/fetchers/test_wordpress.py
+++ tests/fetchers/test_wordpress.py
@@ -3,16 +3,21 @@ from efictopub.fetchers import wordpress
 
 from tests.fixtures.real import wordpress_chapter_html_real_1
 from tests.fixtures.real import wordpress_chapter_html_real_2
-from tests.fixtures.stubs import stub_response
 
 
 class TestFetchersWordpress:
     def setup_method(self):
         config.config["fetcher_opts"] = ["title='Great Story'", "author='Great Author'"]
 
-    def test_fetch_blog_entries(self):
-        stub_response(wordpress_chapter_html_real_1)
-        stub_response(wordpress_chapter_html_real_2)
+    def test_fetch_blog_entries(self, requests_mock):
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/11/1-1/",
+            text=wordpress_chapter_html_real_1,
+        )
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/14/chapter-1-2/",
+            text=wordpress_chapter_html_real_2,
+        )
 
         fetcher = wordpress.Fetcher("https://blog-name.wordpress.com/2011/06/11/1-1/")
         entries = fetcher.fetch_blog_entries()
@@ -26,9 +31,15 @@ class TestFetchersWordpress:
         assert [entry.date_published for entry in entries] == [1307768479, 1307768479]
         assert [entry.date_updated for entry in entries] == [1412115014, None]
 
-    def test_fetch_story(self):
-        stub_response(wordpress_chapter_html_real_1)
-        stub_response(wordpress_chapter_html_real_2)
+    def test_fetch_story(self, requests_mock):
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/11/1-1/",
+            text=wordpress_chapter_html_real_1,
+        )
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/14/chapter-1-2/",
+            text=wordpress_chapter_html_real_2,
+        )
 
         fetcher = wordpress.Fetcher("https://blog-name.wordpress.com/2011/06/11/1-1/")
         story = fetcher.fetch_story()
@@ -39,14 +50,16 @@ class TestFetchersWordpress:
         )
         assert story.author == "author-name"
 
-    def test_last_chapter_pattern(self):
+    def test_last_chapter_pattern(self, requests_mock):
         config.config["fetcher_opts"] = [
             "title='Great Story'",
             "author='Great Author'",
             "last_chapter_pattern=2011/06/11",
         ]
-
-        stub_response(wordpress_chapter_html_real_1)
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/11/1-1/",
+            text=wordpress_chapter_html_real_1,
+        )
 
         fetcher = wordpress.Fetcher("https://blog-name.wordpress.com/2011/06/11/1-1/")
         entries = fetcher.fetch_blog_entries()
@@ -57,14 +70,16 @@ class TestFetchersWordpress:
             '<p dir="ltr">Chapter 1 content here\n</p>'
         ]
 
-    def test_comments(self):
+    def test_comments(self, requests_mock):
         config.config["fetcher_opts"] = [
             "title='Great Story'",
             "author='Great Author'",
             "last_chapter_pattern=2011/06/11",
         ]
-
-        stub_response(wordpress_chapter_html_real_1)
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/11/1-1/",
+            text=wordpress_chapter_html_real_1,
+        )
 
         fetcher = wordpress.Fetcher("https://blog-name.wordpress.com/2011/06/11/1-1/")
         entry = fetcher.fetch_blog_entries()[0]
@@ -81,15 +96,17 @@ class TestFetchersWordpress:
         assert entry.comments[1].author == "commenter 4"
         assert entry.comments[1].text == "<p>comment 4 text</p>"
 
-    def test_comments_only_author(self):
+    def test_comments_only_author(self, requests_mock):
         config.config["fetcher_opts"] = [
             "title='Great Story'",
             "author='Great Author'",
             "last_chapter_pattern=2011/06/11",
             "author_only_replies=commenter 2",
         ]
-
-        stub_response(wordpress_chapter_html_real_1)
+        requests_mock.get(
+            "https://blog-name.wordpress.com/2011/06/11/1-1/",
+            text=wordpress_chapter_html_real_1,
+        )
 
         fetcher = wordpress.Fetcher("https://blog-name.wordpress.com/2011/06/11/1-1/")
         entry = fetcher.fetch_blog_entries()[0].as_chapter()
