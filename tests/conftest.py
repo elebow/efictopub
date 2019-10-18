a327da11be1ea204d3492f7f9c2f3c5adcb28fe5
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-18 01:10:34 -0400

    Remove homegrown request stubbing
    :waves_fondly:

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,9 @@ from efictopub import config
 
 import efictopub.lib.reddit_util
 import efictopub.lib.request_dispatcher
-import tests.fixtures.stubs
 
+
+# requests
 
 if os.environ.get("LIVE_REQUESTS") != "true":
 
@@ -19,27 +20,15 @@ if os.environ.get("LIVE_REQUESTS") != "true":
     def no_reddit():
         efictopub.lib.reddit_util.setup_reddit = MagicMock()
 
-    @pytest.fixture(autouse=True, scope="session")
-    def no_requests():
-        requests.get = tests.fixtures.stubs.request_get
-
     efictopub.lib.request_dispatcher.MIN_DELAY = 0
 
 
-@pytest.fixture(autouse=True, scope="function")
-def empty_stubbed_responses():
-    yield
-
-    num_leftover_stubbed_responses = len(tests.fixtures.stubs.responses)
-
-    # reset the responses so later functions can still pass, if the follower assertion fails
-    tests.fixtures.stubs.responses = []
-
-    # ensure stubbed http responses are all used up at the end of every case
-    assert num_leftover_stubbed_responses == 0
-
+# config
 
 config.load({}, fetcher=None)
+
+
+# fixtures and factories
 
 import factory
 from tests import factories
