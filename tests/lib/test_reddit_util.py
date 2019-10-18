e896515b68041727f60176fb10968566c75f33dc
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-17 21:34:42 -0400

    Update test_reddit_util to use pytest-style fixtures

--- tests/lib/test_reddit_util.py
+++ tests/lib/test_reddit_util.py
@@ -1,20 +1,13 @@
 import pytest
-from unittest.mock import MagicMock
-from unittest.mock import patch
 
 from efictopub.lib import reddit_util
 from efictopub.models.reddit import RedditSubmission, RedditComment, RedditWikiPage
 from efictopub import exceptions
 
 
-subreddit_inst = MagicMock()
-subreddit_class = MagicMock(return_value=subreddit_inst)
-
-
 class TestRedditUtil:
     def setup_method(self):
         self.subject = reddit_util
-        self.praw_reddit = MagicMock()
 
     def test_redditor_name_from_url(self):
         assert (
@@ -33,35 +26,42 @@ class TestRedditUtil:
             == "redditor1"
         )
 
-    @patch("efictopub.lib.reddit_util.parse_url")
-    def test_parse_id_or_url_submission(self, parse_url):
+    def test_parse_id_or_url_submission(self, mocker):
+        mocker.patch("efictopub.lib.reddit_util.parse_url")
+
         submission_url = "https://www.reddit.com/r/great_subr/comments/000/great_subm"
-        self.subject.parse_id_or_url(submission_url, self.praw_reddit)
-        self.subject.parse_url.assert_called_once_with(submission_url, self.praw_reddit)
+        self.subject.parse_id_or_url(submission_url, "praw_reddit object")
 
-    @patch("efictopub.lib.reddit_util.parse_url")
-    def test_parse_id_or_url_comment(self, parse_url):
+        self.subject.parse_url.assert_called_once_with(submission_url, mocker.ANY)
+
+    def test_parse_id_or_url_comment(self, mocker):
+        mocker.patch("efictopub.lib.reddit_util.parse_url")
+
         comment_url = "https://www.reddit.com/r/great_subr/comments/000/great_subm/999"
-        self.subject.parse_id_or_url(comment_url, self.praw_reddit)
-        self.subject.parse_url.assert_called_once_with(comment_url, self.praw_reddit)
+        self.subject.parse_id_or_url(comment_url, "praw_reddit object")
 
-    @patch("efictopub.lib.reddit_util.parse_url")
-    def test_parse_id_or_url_wikipage(self, parse_url):
+        self.subject.parse_url.assert_called_once_with(comment_url, mocker.ANY)
+
+    def test_parse_id_or_url_wikipage(self, mocker):
+        mocker.patch("efictopub.lib.reddit_util.parse_url")
+
         wikipage_url = "reddit.com/r/whatever/wiki/pagename"
-        self.subject.parse_id_or_url(wikipage_url, self.praw_reddit)
-        self.subject.parse_url.assert_called_once_with(wikipage_url, self.praw_reddit)
+        self.subject.parse_id_or_url(wikipage_url, "praw_reddit object")
 
-    @patch("efictopub.lib.reddit_util.parse_url")
-    def test_parse_id_or_url_ambiguous(self, parse_url):
-        # ambiguous id
+        self.subject.parse_url.assert_called_once_with(wikipage_url, mocker.ANY)
+
+    def test_parse_id_or_url_ambiguous(self, mocker):
+        mocker.patch("efictopub.lib.reddit_util.parse_url")
+
         with pytest.raises(exceptions.AmbiguousIdError):
-            self.subject.parse_id_or_url("aaaaaa", self.praw_reddit)
+            self.subject.parse_id_or_url("aaaaaa", "praw_reddit object")
 
-    @patch("praw.models.Submission")
-    @patch("praw.models.Comment")
-    @patch("praw.models.WikiPage")
-    @patch("praw.models.Subreddit", subreddit_class)
-    def test_parse_url(self, wikipage_class, comment_class, submission_class):
+    def test_parse_url(self, mocker):
+        praw_submission_class = mocker.patch("praw.models.Submission")
+        praw_comment_class = mocker.patch("praw.models.Comment")
+        praw_wikipage_class = mocker.patch("praw.models.WikiPage")
+        praw_subreddit_class = mocker.patch("praw.models.Subreddit")
+
         submission_url = (
             "https://reddit.com/r/my_great_subreddit/comments/a123/my_great_title/"
         )
@@ -70,17 +70,17 @@ class TestRedditUtil:
         )
         wiki_url = "https://reddit.com/r/my_great_subreddit/wiki/some/page/name"
 
-        result = self.subject.parse_url(submission_url, self.praw_reddit)
+        result = self.subject.parse_url(submission_url, "praw_reddit object")
         assert isinstance(result, RedditSubmission)
-        submission_class.assert_called_once_with(self.praw_reddit, url=submission_url)
+        praw_submission_class.assert_called_once_with(mocker.ANY, url=submission_url)
 
-        result = self.subject.parse_url(comment_url, self.praw_reddit)
+        result = self.subject.parse_url(comment_url, "praw_reddit object")
         assert isinstance(result, RedditComment)
-        comment_class.assert_called_once_with(self.praw_reddit, url=comment_url)
+        praw_comment_class.assert_called_once_with(mocker.ANY, url=comment_url)
 
-        result = self.subject.parse_url(wiki_url, self.praw_reddit)
+        result = self.subject.parse_url(wiki_url, "praw_reddit object")
         assert isinstance(result, RedditWikiPage)
-        subreddit_class.assert_called_once_with(self.praw_reddit, "my_great_subreddit")
-        wikipage_class.assert_called_once_with(
-            self.praw_reddit, subreddit_inst, "some/page/name"
+        praw_subreddit_class.assert_called_once_with(mocker.ANY, "my_great_subreddit")
+        praw_wikipage_class.assert_called_once_with(
+            mocker.ANY, praw_subreddit_class(), "some/page/name"
         )
