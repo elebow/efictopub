474be59a23f21da794ad4084d0e02ae0589754d9
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-18 21:24:11 -0400

    Convert more unittest-style mocks to pytest-style

--- tests/test_efictopub.py
+++ tests/test_efictopub.py
@@ -1,62 +1,78 @@
-from unittest.mock import MagicMock
-from unittest.mock import patch
+import pytest
 
 from efictopub.efictopub import Efictopub
 
 
-@patch("efictopub.efictopub.Efictopub.fetch_story", MagicMock())
 class TestEfictopub:
-    @patch("efictopub.efictopub.Efictopub.get_fetcher", MagicMock())
-    @patch("efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True)
-    @patch("efictopub.archive.store")
-    @patch("efictopub.git.previous_commit_is_not_efic")
-    def test_archive_story(self, archive_store, previous_commit_not_efic):
-        efictopub = Efictopub()
-        efictopub.archive_story()
+    @pytest.fixture(autouse=True)
+    def fetch_story(self, mocker):
+        mocker.patch("efictopub.efictopub.Efictopub.fetch_story")
+
+    def test_archive_story(self, mocker):
+        mocker.patch("efictopub.efictopub.Efictopub.get_fetcher")
+        mocker.patch(
+            "efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True
+        )
+        archive_store = mocker.patch("efictopub.archive.store")
+        previous_commit_not_efic = mocker.patch(
+            "efictopub.git.previous_commit_is_not_efic"
+        )
+
+        Efictopub().archive_story()
 
         archive_store.assert_called_once()
         previous_commit_not_efic.assert_called_once()
 
-    @patch("efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True)
-    @patch("efictopub.fetchers.fetcher_by_name")
-    def test_get_fetcher_manual(self, fetcher_by_name):
+    def test_get_fetcher_manual(self, mocker):
+        mocker.patch(
+            "efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True
+        )
+        fetcher_by_name = mocker.patch("efictopub.fetchers.fetcher_by_name")
+
         Efictopub({"target": "www.example.com/great-story", "fetcher": "reddit_next"})
 
         fetcher_by_name.assert_called_once_with(
             "reddit_next", "www.example.com/great-story"
         )
 
-    @patch("efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True)
-    @patch("efictopub.fetchers.fetcher_for_url")
-    def test_get_fetcher_auto(self, fetcher_for_url):
+    def test_get_fetcher_auto(self, mocker):
+        mocker.patch(
+            "efictopub.efictopub.Efictopub.check_repo_ready_for_write", lambda _x: True
+        )
+        fetcher_for_url = mocker.patch("efictopub.fetchers.fetcher_for_url")
+
         Efictopub({"target": "www.example.com/great-story"})
 
         fetcher_for_url.assert_called_once_with("www.example.com/great-story")
 
-    @patch("efictopub.git.repo_is_dirty", lambda: False)
-    @patch("efictopub.efictopub.Efictopub.get_fetcher", MagicMock())
-    def test_check_repo_ready_for_write_repo_clean(self):
+    def test_check_repo_ready_for_write_repo_clean(self, mocker):
+        mocker.patch("efictopub.git.repo_is_dirty", lambda: False)
+        mocker.patch("efictopub.efictopub.Efictopub.get_fetcher")
+
         efictopub = Efictopub({"write_archive": True, "clobber": False})
 
         assert efictopub.check_repo_ready_for_write() is True
 
-    @patch("efictopub.git.repo_is_dirty", lambda: True)
-    @patch("efictopub.efictopub.Efictopub.get_fetcher", MagicMock())
-    def test_check_repo_ready_for_write_repo_dirty(self):
+    def test_check_repo_ready_for_write_repo_dirty(self, mocker):
+        mocker.patch("efictopub.git.repo_is_dirty", lambda: True)
+        mocker.patch("efictopub.efictopub.Efictopub.get_fetcher")
+
         efictopub = Efictopub({"write_archive": True, "clobber": False})
 
         assert efictopub.check_repo_ready_for_write() is False
 
-    @patch("efictopub.git.repo_is_dirty", lambda: True)
-    @patch("efictopub.efictopub.Efictopub.get_fetcher", MagicMock())
-    def test_check_repo_ready_for_write_repo_dirty_no_write(self):
+    def test_check_repo_ready_for_write_repo_dirty_no_write(self, mocker):
+        mocker.patch("efictopub.git.repo_is_dirty", lambda: True)
+        mocker.patch("efictopub.efictopub.Efictopub.get_fetcher")
+
         efictopub = Efictopub({"write_archive": False, "clobber": False})
 
         assert efictopub.check_repo_ready_for_write() is True
 
-    @patch("efictopub.git.repo_is_dirty", lambda: True)
-    @patch("efictopub.efictopub.Efictopub.get_fetcher", MagicMock())
-    def test_check_repo_ready_for_write_repo_dirty_clobber(self):
+    def test_check_repo_ready_for_write_repo_dirty_clobber(self, mocker):
+        mocker.patch("efictopub.git.repo_is_dirty", lambda: True)
+        mocker.patch("efictopub.efictopub.Efictopub.get_fetcher")
+
         efictopub = Efictopub({"write_archive": True, "clobber": True})
 
         assert efictopub.check_repo_ready_for_write() is True
