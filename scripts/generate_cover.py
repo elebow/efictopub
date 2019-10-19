b4b0b05dac7a8a3d154459c9360d689762ba70c9
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-19 01:08:15 -0400

    Add generate_cover.py script for testing cover SVGs

---
+++ scripts/generate_cover.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# PYTHONPATH=../efictopub ./scripts/generate_cover.py cover.svg
+
+import sys
+
+import efictopub
+from efictopub.models.chapter import Chapter
+from efictopub.models.story import Story
+
+efictopub.config.load({}, fetcher=None)
+
+chapters = [
+    Chapter(
+        comments=[],
+        date_published=5,
+        date_updated=6,
+        permalink="http://example.com/story5",
+        score=7,
+        text="chapter text",
+        title="chapter title",
+    )
+]
+story = Story(title="My Great Story", author="Author Name", chapters=chapters)
+svg = story.cover_svg
+
+with open(sys.argv[1], "w") as outfile:
+    outfile.write(svg)
