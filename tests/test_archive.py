0313a1ba1892b53e044cab83d3651ec875396e6d
Eddie Lebow <elebow@users.noreply.github.com>
2019-10-18 22:01:32 -0400

    Convert more unittest-style mocks to pytest-style

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,9 +1,4 @@
 import pytest
-from unittest.mock import call
-from unittest.mock import MagicMock
-from unittest.mock import mock_open
-from unittest.mock import patch
-
 
 from efictopub import archive
 from efictopub import config
@@ -11,52 +6,59 @@ from efictopub.exceptions import NoArchivedStoryError, AmbiguousArchiveIdError
 
 
 class TestArchive:
-    def setup_class(self):
-        self.id = "55555-my-great-story-id"
+    ID = "55555-my-great-story-id"
 
-    @patch("efictopub.archive.id_or_path_to_path", lambda x: "my-great-path")
-    @patch("jsonpickle.decode")
-    @patch("builtins.open", new_callable=mock_open)
-    def test_get(self, file_open, jsonpickle_decode):
-        archive.get(self.id)
+    def test_get(self, mocker):
+        mocker.patch("efictopub.archive.id_or_path_to_path", lambda x: "my-great-path")
+        jsonpickle_decode = mocker.patch("jsonpickle.decode")
+        mocker.patch("builtins.open", new_callable=mocker.mock_open)
 
-        file_open.assert_called_once_with("my-great-path", "r")
-        jsonpickle_decode.assert_called_once_with(file_open().read())
+        archive.get(self.ID)
 
-    @patch("efictopub.git.commit_story")
-    @patch("jsonpickle.encode")
-    @patch("builtins.open", new_callable=mock_open)
-    def test_store(self, file_open, jsonpickle_encode, git_commit_story):
-        story = MagicMock(id="my-great-path", title="great-story", text="hhh")
+        open.assert_called_once_with("my-great-path", "r")
+        jsonpickle_decode.assert_called_once_with(open().read())
+
+    def test_store(self, mocker):
+        git_commit_story = mocker.patch("efictopub.git.commit_story")
+        jsonpickle_encode = mocker.patch("jsonpickle.encode")
+        mocker.patch("builtins.open", new_callable=mocker.mock_open)
+
+        story = mocker.MagicMock(id="my-great-path", title="great-story", text="hhh")
         config.config["archive_location"] = "/path/to/archive"
         archive.store(story)
 
-        file_open.assert_called_once_with("/path/to/archive/my-great-path.json", "w")
+        open.assert_called_once_with("/path/to/archive/my-great-path.json", "w")
         jsonpickle_encode.assert_called_once_with(story)
         git_commit_story.assert_has_calls(
             [
-                call(story, "Local changes before fetching great-story"),
-                call(story, "Fetch great-story"),
+                mocker.call(story, "Local changes before fetching great-story"),
+                mocker.call(story, "Fetch great-story"),
             ]
         )
 
-    @patch("glob.glob", lambda x: [])
-    def test_path_zero_matches_found(self):
-        with pytest.raises(NoArchivedStoryError):
-            archive.id_or_path_to_path(self.id)
+    def test_path_zero_matches_found(self, mocker):
+        mocker.patch("glob.glob", lambda x: [])
 
-    @patch("glob.glob", lambda x: ["/whatever/path/to/55555-my-great-story-id.json"])
-    def test_path_one_match_found(self):
+        with pytest.raises(NoArchivedStoryError):
+            archive.id_or_path_to_path(self.ID)
+
+    def test_path_one_match_found(self, mocker):
+        mocker.patch(
+            "glob.glob", lambda x: ["/whatever/path/to/55555-my-great-story-id.json"]
+        )
+
         assert (
-            archive.id_or_path_to_path(self.id)
+            archive.id_or_path_to_path(self.ID)
             == "/whatever/path/to/55555-my-great-story-id.json"
         )
 
-    @patch("glob.glob", lambda x: ["/whatever/1", "/whatever/2"])
-    def test_path_many_matches_found(self):
-        with pytest.raises(AmbiguousArchiveIdError):
-            archive.id_or_path_to_path(self.id)
+    def test_path_many_matches_found(self, mocker):
+        mocker.patch("glob.glob", lambda x: ["/whatever/1", "/whatever/2"])
 
-    @patch("glob.glob", lambda x: ["55555-my-great-story-id.json"])
-    def test_path_partial_id(self):
+        with pytest.raises(AmbiguousArchiveIdError):
+            archive.id_or_path_to_path(self.ID)
+
+    def test_path_partial_id(self, mocker):
+        mocker.patch("glob.glob", lambda x: ["55555-my-great-story-id.json"])
+
         assert archive.id_or_path_to_path("55") == "55555-my-great-story-id.json"
