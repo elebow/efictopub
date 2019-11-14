82e9ed050de27deda7e6500c6ff25bb0bb57bd71
Eddie Lebow <elebow@users.noreply.github.com>
2019-11-13 21:37:18 -0500

    Update example_client_app.py to be more explanatory
    Also, instantiating the Efictopub no longer performs the fetch.

--- docs/example_client_app.py
+++ docs/example_client_app.py
@@ -5,18 +5,18 @@ args = {
     "title": "My Great Story",
 }
 
-# instantiate the Efictopub object. This fetches the story.
+# instantiate the Efictopub object.
 efictopub = Efictopub(args)
 
-# write the story to the archive
+# The Story object. Referencing this lazily fetches the story from the source.
+efictopub.story
+
+# write the story to the archive. This will fetch the story, if it hasn't been yet.
 efictopub.archive_story()
 
-# write an EPUB file
+# write an EPUB file. This will fetch the story, if it hasn't been yet.
 efictopub.write_epub()
 
-# return EPUB file contents without writing to disk
+# return EPUB file contents without writing to disk. This will fetch the story, if it hasn't been yet.
 # NYI
 efictopub.epub()
-
-# return the Story object
-efictopub.story
